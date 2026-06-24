47833033ca7c9d5556d28a5b1bfa6f5c7777c810
gu4rdian059-star <gu4rdian059@gmail.com>
2026-06-24 08:14:12 +0700

    Delete lib/config directory

--- lib/config/supabase_config.dart
+++
@@ -1,8 +0,0 @@
-/// Supabase project credentials for SekolahPRO
-class SupabaseConfig {
-  static const String supabaseUrl = 'https://vrecioikvfkubxozyzgy.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
-      '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyZWNpb2lrdmZrdWJ4b3p5emd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NTc0OTQsImV4cCI6MjA5NTIzMzQ5NH0'
-      '.2d51rlTDT6uBOW2keS-1dohK9c3LlxCpYdAc7pRiK70';
-}
